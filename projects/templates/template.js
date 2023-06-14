3941c11142294ae1765bc135dc5c6beaa2df523e
Franz Alarcon <franzalarconv@gmail.com>
2023-06-13 22:21:50 -0700

    Develop CONU project page

---
+++ projects/templates/template.js
@@ -0,0 +1,14 @@
+import { ImageGallery } from "/scripts/ImageGallery.js"
+import { VirtualTourBuilder } from "/scripts/PageBuilder.js"
+
+"use strict"
+
+let pageBuilder = new VirtualTourBuilder();
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    // Build virtual tour
+    // pageBuilder.buildVirtualTour("*TEMP*");
+    // pageBuilder.loadEvents("*TEMP*");
+}
