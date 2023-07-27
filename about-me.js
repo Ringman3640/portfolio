d516d40d432446727b63c589be823ceaddcb9092
Franz Alarcon <franzalarconv@gmail.com>
2023-07-26 21:27:26 -0700

    Complete Why Computer Science section

--- about-me.js
+++ about-me.js
@@ -1,10 +1,18 @@
 import { buildNavBar } from "/scripts/PageBuilder.js"
 import { fadeInPage, addAnchorFadeOut } from "/scripts/ElementUtilities.js"
+import { ImageGallery } from "/scripts/ImageGallery.js"
+
+let logicGallery = null;
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     buildNavBar();
+
+    logicGallery = new ImageGallery(document.getElementById("logic-gallery"))
+    logicGallery.loadImages(
+            "/resources/images/about-me/logic-gallery/logic-gallery.json");
+
     addAnchorFadeOut();
     fadeInPage();
 }
