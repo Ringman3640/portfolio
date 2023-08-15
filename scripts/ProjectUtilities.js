03a770152eb2220cad6a6f600c26159276cb2fea
Franz Alarcon <franzalarconv@gmail.com>
2023-08-14 22:45:19 -0700

    Add getFavoriteProjects

--- scripts/ProjectUtilities.js
+++ scripts/ProjectUtilities.js
@@ -3,6 +3,7 @@
 // Author: Franz Alarcon
 
 let projectFileURL = "/resources/project-info/project-info.json";
+let projectJson = null;
 
 // getAllProjects function
 // Retrieves information from all projects. 
@@ -17,20 +18,55 @@ let projectFileURL = "/resources/project-info/project-info.json";
 //      - completeYear: Year that the project was completed or first published
 //      - thumbnailURL: URL to a thumbnail image
 async function getAllProjects() {
+    if (!projectJson) {
+        await loadProjectJson();
+    }
+
+    return projectJson.projects;
+}
+
+// getFavoriteProjects function
+// Retrieves information from all favorite projects.
+// Returns an array of the listed favorite projects, each containing the same
+//     attributes as detailed in getAllProjects.
+async function getFavoriteProjects() {
+    if (!projectJson) {
+        await loadProjectJson();
+    }
+    
+    let favoriteList = [];
+    for (let projectNameObj of projectJson.favoriteProjects) {
+        let projectName = projectNameObj.name;
+        for (let project of projectJson.projects) {
+            if (project.name != projectName) {
+                continue;
+            }
+
+            favoriteList.push(project);
+            break;
+        }
+    }
+
+    return favoriteList;
+}
+
+// loadProjectJson function
+// Loads the project Json file to projectJson.
+// Helper function for getAllProjects and getFavoriteProjects.
+async function loadProjectJson() {
     let response = await fetch(projectFileURL);
     if (!response.ok) {
         throw new Error("getAllProjects: Could not fetch project JSON file.");
     }
 
-    let json;
     try {
-        json = await response.json();
+        projectJson = await response.json();
     } catch(error) {
         throw new Error("getAllProjects: Could not read project file as JSON.");
     }
-    return json.projects;
 }
 
 export {
-    getAllProjects
+    getAllProjects,
+    getFavoriteProjects
 }
